ca7db1f1cc32fc1e410787b5b0b31f33a1e27877
Bishwas <bishwasbisu@gmail.com>
2023-06-30 01:21:48 +0545

    After getting everything ready except for the gallery

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,8 @@
+//AOS for animating on scroll
+AOS.init();
+
+// Swiper for the hero sections sliders
+
 var swiper = new Swiper(".myHeroSwiper", {
     pagination: {
       el: ".swiper-pagination",
